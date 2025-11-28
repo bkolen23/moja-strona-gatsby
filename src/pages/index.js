3f8548dae787dad060996d676812ad1cb2eb009e
bkolen23 <b.kolender23@zsek.olsztyn.pl>
2025-11-28 11:22:07 +0100

    Tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Strona główna">
       <p>Robię tę stronę przy pomocy poradnika Gatsby</p>
+      <StaticImage
+        alt="Pies"
+        src="../images/dog.jpg"
+      />
     </Layout>
   )
 }
