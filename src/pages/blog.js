2a17f588c1bce21c4ee882ea1ea5607f72800075
bkolen23 <b.kolender23@zsek.olsztyn.pl>
2025-11-30 22:50:21 +0100

    tutorial part 4

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,33 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <p>Moje posty pojawią się tutaj</p>
+            <ul>
+            {
+                data.allFile.nodes.map(node =>(
+                    <li key={node.name}>
+                        {node.name}
+                    </li>
+                ))
+            }
+            </ul>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query {
+        allFile {
+            nodes {
+                name
+            }
+        }
+    }
+`
 
 export const Head = () => <Seo title="Mój Blog"/>
 
